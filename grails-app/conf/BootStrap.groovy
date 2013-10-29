2f57ac13edbacc912351fdd0bb07d2c6a77b38a1
Paula Montero <pau.montero88@gmail.com>
2013-10-29 20:35:29 -0300

    Acomodando algunas cosas del proyecto

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,13 +4,24 @@ class BootStrap {
 
 	def init = { servletContext ->
 
-		new Usuario(nombre: "Maria", apellido: "Lopez", dni: 33157053 , email: "marialopez@gmail.com", telefono: 35282463 , contrasenia: 'abc123').save(flush:true)
+		def usuario1 = new Usuario(nombre: "Maria", apellido: "Lopez", dni: 33157053 , email: "marialopez@gmail.com", telefono: 35282463 , contrasenia: 'abc123')
+		usuario1.save()
+		
+		println usuario1
+		
 
-		//new Usuario(nombre: "Lucia", apellido: "Roldan", dni: 29157077 , email: "luciaroldan@gmail.com", telefono: 44414566 , contrasenia: 'def456').save()
+		def usuario2 = new Usuario(nombre: "Lucia", apellido: "Roldan", dni: 29157077 , email: "luciaroldan@gmail.com", telefono: 44414566 , contrasenia: 'def456')
+		usuario2.save()
+		
+		println usuario2
+		
 
-		//new Usuario(nombre: "Marcos", apellido: "Suarez", dni: 27100732 , email: "marcossuarez@gmail.com", telefono: 15309921 , contrasenia: 'ghi000').save()
-
-		//new Usuario(nombre: "Marcos", apellido: "Suarez", dni: 27100732 , email: "ms@gmail.com", telefono: 153099011 , contrasenia: 'aaa111').save()//repetido
+		def usuario3 = new Usuario(nombre: "Marcos", apellido: "Suarez", dni: 27100732 , email: "marcossuarez@gmail.com", telefono: 15309921 , contrasenia: 'ghi000')
+		usuario3.save()
+		
+		println usuario3
+		
+		
 	}
 
 	def destroy = {
